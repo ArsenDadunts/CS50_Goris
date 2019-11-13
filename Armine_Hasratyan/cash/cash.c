535168261ee3c56b0063f3552991d17c5c4065bd
Armine <armine_has@instigatemobile.com>
2019-11-13 12:14:22 +0000

    Add less mario

--- Armine_Hasratyan/cash/cash.c
+++ Armine_Hasratyan/cash/cash.c
@@ -3,39 +3,36 @@
 #include <math.h>
 
 int main(void)
-
 {
     float dollars;
-    do 
+    do
     {
         dollars = get_float("Change owed: ");
-    } 
+    }
     while (dollars < 0);
-}
 
-{
     int cents = round(dollars * 100);
-    int coins;
-    
-    while (cents >= 25)         
+    int coins = 0;
+
+    while (cents >= 25)
     {
         cents = cents - 25;
-        cents++;
+        coins++;
     }
-    while (cents >= 10) 
+    while (cents >= 10)
     {
         cents = cents - 10;
-        cents++;
+        coins++;
     }
-    while (cents >= 5) 
+    while (cents >= 5)
     {
         cents = cents - 5;
-        cents++;
+        coins++;
     }
-    while (cents >= 1) 
+    while (cents >= 1)
     {
         cents = cents - 1;
-        cents++;
+        coins++;
     }
-    return coins;
+    printf("%i\n", coins);
 }
